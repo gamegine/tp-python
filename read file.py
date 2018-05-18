c3b0fc18e865302f8f2f1b1c8ab1b8949b72d556
gamgine <victor.fromentin95@ynov.com>
2018-05-18 11:46:14 +0200

    filtre

--- read file.py
+++ read file.py
@@ -1,6 +1,12 @@
 import csv
-with open('autolib-disponibilite-temps-reel.csv', 'r') as csvfile:
+with open('autolib-disponibilite-temps-reel.csv', 'r',encoding="utf8") as csvfile:
 #csvfile = open('autolib-disponibilite-temps-reel.csv', 'r')
+    ar ={}
     for row in csv.reader(csvfile, delimiter=';'):
-        if row[12].startswith("75"):
-            print(row[1],row[12])
+        if row[12].startswith("750"):
+            #print(row[1],row[12])
+            if not row[12] in ar:
+                ar[row[12]]=int(row[1])
+            else:
+                ar[row[12]]+=int(row[1])
+    print(ar)
